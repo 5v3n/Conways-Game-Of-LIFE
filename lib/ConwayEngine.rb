b9a0c93a1e80e99c357d16936e827a01692baa93
Sven Kraeuter <sven.kraeuter@web.de>
2010-01-18 13:02:06 +0100

    Revert "changed cell's state. was: true,false; is: :alive,:dead. Reduces performance about 20%"
    This reverts commit 225836e6854ca2fe960185535e5a04741336618a.

--- lib/ConwayEngine.rb
+++ lib/ConwayEngine.rb
@@ -13,22 +13,22 @@ The initial pattern constitutes the seed of the system. The first generation is 
 
   attr_accessor :cells
 #Create a new ConwayEngine. Pass width and height of the cell world
-def initialize(width=30, height=30, random=true, random_from=[:alive,:dead].to_a)
+def initialize(width=30, height=30, random=true, random_from=[true,false].to_a)
   #create an array 
   @cells = Array.new()
-  border = :dead
+  border = false
   
    #add 'height' arrays of size 'width'
   height.times do
     if random # fill it with random arrays   
-      new_element =  (0...width).map{random_from[rand(random_from.size)]} #random cohoice between elements
+      new_element =  (0...width).map{random_from[rand(random_from.size)]} #random cohoice between [true,false]
     
       #fill  borders 
       new_element[0]=border
       new_element[-1]=border
     else # fill it with overcrowded
       new_element = Array.new(width, border) # fill it with arrays 
-      new_element[1...-1]=Array.new(new_element.size-2,:alive) #fill except borders
+      new_element[1...-1]=Array.new(new_element.size-2,true) #fill orders
     end
     @cells.push new_element
   end
@@ -44,16 +44,15 @@ end
 def printCells(array)
   array.each do |row|
     row.each do |element|
-      print "o" if element == :alive
-      print "." if element == :dead
+      print "o" if element == true
+      print "." if element == false
     end
      print "\n"
   end
 end
 # calculate the generation n+1 
 def iterate()
-  new_cells = Array.new(@cells.size,:dead).map!{ Array.new(@cells.first.size,:dead) }
- 
+   new_cells = Array.new(@cells.size,false).map!{ Array.new(@cells.first.size,false) }
    @cells.each_with_index do |row, y|
      if(y > 0 and y <@cells.size-1) #leave border out
        row.each_with_index do |entry, x|
@@ -64,7 +63,6 @@ def iterate()
        end
      end
    end
-
    @cells = new_cells.clone
 end
 
@@ -72,32 +70,32 @@ end
 #Count the neighbors of a 2d orthogonal grid. Uses the concept of the N_8(P)-neighborhood (Moore-neighborhood)
 def count_neighbors(x,y,cells)
   counter=0
-  counter += 1 if cells[x+1][y-1] == :alive
-  counter += 1 if cells[x+1][y] == :alive
-  counter += 1 if cells[x+1][y+1] == :alive
-  counter += 1 if cells[x][y+1] == :alive
-  counter += 1 if cells[x-1][y+1] == :alive
-  counter += 1 if cells[x-1][y] == :alive
-  counter += 1 if cells[x-1][y-1] == :alive
-  counter += 1 if cells[x][y-1] == :alive
+  counter += 1 if cells[x+1][y-1]
+  counter += 1 if cells[x+1][y]  
+  counter += 1 if cells[x+1][y+1]
+  counter += 1 if cells[x][y+1]
+  counter += 1 if cells[x-1][y+1]
+  counter += 1 if cells[x-1][y]
+  counter += 1 if cells[x-1][y-1] 
+  counter += 1 if cells[x][y-1]
 
   counter
 end
 #determine the n+1 state of a single cell according to it's neighbors count
 def evolve(cell, neighbors)
   new_cell = cell
-  if cell==:alive
+  if cell==true
     case
     when neighbors < 2
-      new_cell = :dead #  1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+      new_cell = false #  1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
     when neighbors > 3
-      new_cell = :dead #  2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+      new_cell = false #  2. Any live cell with more than three live neighbours dies, as if by overcrowding.
     when (neighbors == 2 or neighbors == 3)
-      new_cell = :alive #  3. Any live cell with two or three live neighbours lives on to the next generation.
+      new_cell = true #  3. Any live cell with two or three live neighbours lives on to the next generation.
     end
-  elsif cell==:dead
+  elsif cell==false
     if neighbors == 3
-     new_cell = :alive # 4. Any dead cell with exactly three live neighbours becomes a live cell.
+     new_cell = true # 4. Any dead cell with exactly three live neighbours becomes a live cell.
     end
   end
   new_cell
@@ -106,4 +104,50 @@ end
 
 end
 
+if __FILE__ == $0
+  
+  ten_cell_row =[
+    [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+    [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     ]
+  
+           ten_cell_row_next =[
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     ]
+  
+  freshConwayEngine = ConwayEngine.new(120,50)
+  #freshConwayEngine.cells = ten_cell_row
 
+  
+  runs = 500
+  runs.times {freshConwayEngine.iterate()}
+  while true do
+    freshConwayEngine.printCells(freshConwayEngine.cells)
+    puts "Tick #{runs} \nPress 'Q' to quit, return to continue"
+    input = gets
+    break if "Q\n" == input
+    freshConwayEngine.iterate()
+    system("clear")
+    runs += 1
+  end
+  
+end

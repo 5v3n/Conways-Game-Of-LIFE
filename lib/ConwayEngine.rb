f3ee291e974e7424ec91e706cdd775404c11a82c
Sven Kraeuter <sven.kraeuter@web.de>
2010-01-23 02:03:12 +0100

    Refactoring: go for OO & see about the speed

--- lib/ConwayEngine.rb
+++ lib/ConwayEngine.rb
@@ -10,92 +10,102 @@ The universe of the Game of Life is an infinite two-dimensional orthogonal grid 
 
 The initial pattern constitutes the seed of the system. The first generation is created by applying the above rules simultaneously to every cell in the seed—births and deaths happen simultaneously, and the discrete moment at which this happens is sometimes called a tick (in other words, each generation is a pure function of the one before). The rules continue to be applied repeatedly to create further generations.
 =end
-#TODO: private functions - but what about the unit tests??
-  attr_accessor :cells
-#Create a new ConwayEngine. Pass width and height of the cell world
-def initialize(width=30, height=30, random=true, random_from=[true,false].to_a)
-  #create an array 
-  @cells = Array.new()
-  border = false
-  
-   #add 'height' arrays of size 'width'
-  height.times do
-    if random # fill it with random arrays   
-      new_element =  (0...width).map{random_from[rand(random_from.size)]} #random cohoice between [true,false]
-    
-      #fill  borders 
-      new_element[0]=border
-      new_element[-1]=border
-    else # fill it with overcrowded
-      new_element = Array.new(width, border) # fill it with arrays 
-      new_element[1...-1]=Array.new(new_element.size-2,true) #fill orders
-    end
-    @cells.push new_element
-  end
-  
+require_relative 'Grid'
+require_relative 'Cell'
 
-  #draw border
-  @cells[0]= Array.new(width,border)
-  @cells[-1]= Array.new(width,border)
-  
+#TODO: private functions - but what about the unit tests??
+#FIX: x/y in 2d array...
+attr_accessor :grid
+#Create a new ConwayEngine. Pass width and height of the cell world
+def initialize(width, height)
+  @grid   = Grid.new(width, height)
+end
+
+def width()
+  @grid.width
+end
+
+def height()
+  @grid.height
 end
 
  
-def printCells(array)
+def printCells(conway_engine)
+  array = conway_engine.grid.cells #TODO catch 'npe'
   array.each do |row|
-    row.each do |element|
-      print "o" if element == true
-      print "." if element == false
+    row.each do |cell|
+      print "o" if cell.alive?
+      print "." if !cell.alive? 
     end
      print "\n"
   end
 end
 # calculate the generation n+1 
 def iterate()
-   new_cells = Array.new(@cells.size,false).map!{ Array.new(@cells.first.size,false) }
-   @cells.each_with_index do |row, y|
-     if(y > 0 and y <@cells.size-1) #leave border out
+   new_cells = Array.new(self.height, Cell.new(false)).map!{ Array.new(self.width,Cell.new(false)) }
+   puts "before: new: #{new_cells.object_id}, instance: #{@grid.cells.object_id}"
+   
+   @grid.cells.each_with_index do |row, y|
+     if(y > 0 and y <self.height-1) #leave border out
+       row.each_with_index do |entry, x|
+         if(x > 0 and x < self.width-1) #leave border out
+           neighbor_count = count_neighbors(y,x,@grid.cells)
+           new_cells[y][x] = evolve(entry, neighbor_count)
+             puts "in the middle: new: #{new_cells.object_id}, instance: #{@grid.cells.object_id}"
+ 
+         end
+       end
+     end
+   end
+   @grid.cells = new_cells.clone
+   puts "after: new: #{new_cells.object_id}, instance: #{@grid.cells.object_id}"
+
+=begin
+ new_cells = Array.new(@grid.cells.size,Cell.new(false)).map!{ Array.new(@grid.cells.first.size,Cell.new(false)) }
+   @grid.cells.each_with_index do |row, y|
+     if(y > 0 and y <@grid.cells.size-1) #leave border out
        row.each_with_index do |entry, x|
          if(x > 0 and x < row.size-1) #leave border out
-           neighbor_count = count_neighbors(y,x,@cells)
+           neighbor_count = count_neighbors(y,x,@grid.cells)
            new_cells[y][x] = evolve(entry, neighbor_count)
          end
        end
      end
    end
-   @cells = new_cells.clone
+   @grid.cells = new_cells.clone
+=end
 end
 
 
 #Count the neighbors of a 2d orthogonal grid. Uses the concept of the N_8(P)-neighborhood (Moore-neighborhood)
 def count_neighbors(x,y,cells) #TODO: tell don't ask!
   counter=0
-  counter += 1 if cells[x+1][y-1]
-  counter += 1 if cells[x+1][y]  
-  counter += 1 if cells[x+1][y+1]
-  counter += 1 if cells[x][y+1]
-  counter += 1 if cells[x-1][y+1]
-  counter += 1 if cells[x-1][y]
-  counter += 1 if cells[x-1][y-1] 
-  counter += 1 if cells[x][y-1]
+  counter += 1 if cells[x+1][y-1].alive?
+  counter += 1 if cells[x+1][y].alive?  
+  counter += 1 if cells[x+1][y+1].alive?
+  counter += 1 if cells[x][y+1].alive?
+  counter += 1 if cells[x-1][y+1].alive?
+  counter += 1 if cells[x-1][y].alive?
+  counter += 1 if cells[x-1][y-1].alive?
+  counter += 1 if cells[x][y-1].alive?
 
   counter
 end
 #determine the n+1 state of a single cell according to it's neighbors count
 def evolve(cell, neighbors)
   new_cell = cell
-  if cell==true
+  if cell.alive?
     case
     when neighbors < 2
-      new_cell = false #  1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+      new_cell.alive = false #  1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
     when neighbors > 3
-      new_cell = false #  2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+      new_cell.alive = false #  2. Any live cell with more than three live neighbours dies, as if by overcrowding.
     when (neighbors == 2 or neighbors == 3)
-      new_cell = true #  3. Any live cell with two or three live neighbours lives on to the next generation.
+      new_cell.alive = true #  3. Any live cell with two or three live neighbours lives on to the next generation.
     end
-  elsif cell==false
+  elsif !cell.alive?
     if neighbors == 3
-     new_cell = true # 4. Any dead cell with exactly three live neighbours becomes a live cell.
+     new_cell.alive = true # 4. Any dead cell with exactly three live neighbours becomes a live cell.
     end
   end
   new_cell
@@ -104,50 +114,5 @@ end
 
 end
 
-if __FILE__ == $0
-  
-  ten_cell_row =[
-    [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-    [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     ]
-  
-           ten_cell_row_next =[
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
-     ]
-  
-  freshConwayEngine = ConwayEngine.new(120,50)
-  #freshConwayEngine.cells = ten_cell_row
 
   
-  runs = 500
-  runs.times {freshConwayEngine.iterate()}
-  while true do
-    freshConwayEngine.printCells(freshConwayEngine.cells)
-    puts "Tick #{runs} \nPress 'Q' to quit, return to continue"
-    input = gets
-    break if "Q\n" == input
-    freshConwayEngine.iterate()
-    system("clear")
-    runs += 1
-  end
-  
-end
---
+++ lib/Grid.rb
@@ -0,0 +1,43 @@
+# a grid for Conway's game of LIFE
+class Grid
+  attr_accessor :cells, :width, :height
+  def initialize(width=120, height=50, random=true)
+    @width=width
+    @height=height
+    @cells = Array.new()
+    
+    random_seed = [true,false]
+    border = false
+    
+    #add 'height' arrays of size 'width'
+    @height.times do
+      if random # fill it with random arrays   
+        new_element =  (0...@width).map{Cell.new(random_seed[rand(random_seed.size)])} #random choice out of `random_seed`
+        #fill  borders 
+        new_element[0]=Cell.new(border)
+        new_element[-1]=Cell.new(border)
+      else # fill it overcrowded
+        new_element = Array.new(@width, Cell.new(border)) # fill it with arrays 
+        new_element[1...-1]=Array.new(new_element.size-2,Cell.new(true)) #fill borders
+      end
+      @cells.push new_element
+    end
+  
+    #draw border
+    @cells[0]= Array.new(@width,Cell.new(border))
+    @cells[-1]= Array.new(@width,Cell.new(border))
+  end
+  def cells=(new_cells)
+    @cells=new_cells
+    @height=@cells.size
+    @width=@cells.first.size  
+  end
+  # Helper method converts 2d arrays of boolean values to Cell object 2d arrays
+  def self.convertFromBooleanGridtoCellGrid(boolean_grid)
+    boolean_grid.clone.each{|row| row.map! { |entry| entry = Cell.new(entry)}}
+  end
+  # Helper method converts & overwrites 2d arrays of boolean values to Cell object 2d arrays
+  def self.convertFromBooleanGridtoCellGrid!(boolean_grid)
+    boolean_grid.each{|row| row.map! { |entry| entry = Cell.new(entry)}}
+  end
+end

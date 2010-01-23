41cc2f87d2e5d501ab6c07cee97022dc158276ba
Sven Kraeuter <sven.kraeuter@web.de>
2010-01-23 03:50:05 +0100

    compared cleaner_oo and master

--- test/AnalyzeConwayEngine.rb
+++ test/AnalyzeConwayEngine.rb
@@ -3,6 +3,7 @@
 require 'benchmark'
 require_relative '../lib/ConwayEngine.rb'
 
+
 if __FILE__ == $0
  
   freshConwayEngine = ConwayEngine.new(120,50)
@@ -14,6 +15,7 @@ if __FILE__ == $0
   
   if benchmark
     repeats.times do
+      freshConwayEngine = ConwayEngine.new(120,50)
       bm_results << Benchmark.measure {  runs.times {freshConwayEngine.iterate()} } 
     end
   end
@@ -23,17 +25,30 @@ if __FILE__ == $0
     result += entry.real
   end
   
-  
-  
+    ten_cell_row =[
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+     ]
+   
+  tick = 0
   while true do
     freshConwayEngine.printCells(freshConwayEngine.cells)
-    puts "Just did #{runs} ticks repeated #{repeats} times, which took #{result} secs. We're running about #{(repeats*runs) / result} ticks/sec" unless runs==0
-    puts "Tick #{runs} \nPress 'Q' to quit, return to continue" # TODO fix runs number: seperate runs & repeats in benchmark from the actual generation/tick count. count in ConwayEngine?
+    puts "Just did #{runs} ticks repeated #{repeats} times, which took #{result} secs. We're running about #{(repeats*runs) / result} ticks/sec @ #{freshConwayEngine.cells.first.size}x#{freshConwayEngine.cells.size}" if benchmark
+    puts "Tick #{tick} \nPress 'Q' to quit, return to continue" # TODO fix runs number: seperate runs & repeats in benchmark from the actual generation/tick count. count in ConwayEngine?
     input = gets
     break if "Q\n" == input
     freshConwayEngine.iterate()
     system("clear")
-    runs += 1
+    tick += 1
   end
   
 end

2ebdc50f6220da68011fec465cfb9b5b4eef824c
Sven Kräuter <sven.kraeuter@web.de>
2010-03-09 00:26:44 +0100

    used a proper Benchmark.benchmark test for performance measuring

--- test/AnalyzeConwayEngine.rb
+++ test/AnalyzeConwayEngine.rb
@@ -3,9 +3,15 @@
 require 'benchmark'
 require '../lib/ConwayEngine.rb'
 
+ def fps array
+   array.map { |item| 1.0/item unless item==nil }
+ end
+
 
 if __FILE__ == $0
-  repeats = 3
+  repeats = 5
+  repeats = ARGV[0].to_i if ARGV[0]
+  
   # use benchmark with custom results: total & average time
   Benchmark.benchmark(" "*25 + Benchmark::CAPTION, 25, Benchmark::FMTSTR, ">avg small", ">avg big", ">tps small", ">tps big") do |x|
     bmConwayEngine = ConwayEngine.new(120,50)
@@ -24,17 +30,24 @@ if __FILE__ == $0
     
     avg_small = t_small / repeats
     avg_big   = t_big / repeats
+    # divide result with FixNum works, but not vice versa:
     #tps_small = repeats * (1.0 / t_small)
-    #tps_big   = repeats / t_big 
-    
-    [avg_small, avg_big]#, tps_small, tps_big]
-    
-    # divide result with FixNum works, but not vice versa...
+    #tps_big   = repeats / t_big
 
+    fps_avg_small  = fps(avg_small.to_a)
+    fps_avg_big    = fps(avg_big.to_a)
+
+    puts "Average tps small" + " "*10 + "%07f" % fps_avg_small[1] + " "*23 + "(  %07f)" % fps_avg_small.last()
+    puts "Average tps big  " + " "*10 + "%07f" % fps_avg_big[1]   + " "*23 + "(  %07f)" % fps_avg_big.last()
+
+    [avg_small, avg_big]#, fps_avg_small, fps_avg_big]
+    # TODO: format avg ticks per second properly
+    # TODO: use .bmbm (ramping up included)
+ 
    end
 
   
- 
+
    
   
 end
